dfd1c9d2f3635caa467c5c79f1ce02debdcc9ed6
vennylee <hyungsup42134@gmail.com>
2021-12-17 03:17:49 +0900

    Refactoring javascript (1)

--- scripts.js
+++ scripts.js
@@ -6,36 +6,54 @@ const email = document.getElementById("email");
 const password = document.getElementById("password");
 const password2 = document.getElementById("password2");
 
+// Show Error
+function showError(input, message) {
+  const formControl = input.parentNode;
+  formControl.classList.add("error");
+  const small = formControl.querySelector(".small");
+  small.innerText = message;
+}
+
+// Show Success
+function showSuccess(input) {
+  const formControl = input.parentNode;
+  formControl.classList.add("success");
+}
+
+// Email is valid
+function isValidEmail(emailValue) {
+  const re =
+    /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+  return String(emailValue).toLowerCase().match(re);
+}
+
+// Event listener
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-  if (username.value.length < 6) {
-    username.parentNode.classList.add("error");
+
+  if (username.value === "") {
+    showError(username, "username is required");
   } else {
-    username.parentNode.classList.add("success");
+    showSuccess(username);
   }
 
-  if (email.value) {
-    if (email.value.includes("@")) {
-      email.parentNode.classList.add("success");
-    } else {
-      email.parentNode.classList.add("error");
-    }
+  if (email.value === "") {
+    showError(email, "email is required");
+  } else if (!isValidEmail(email.value)) {
+    showError(email, "email is required");
+  } else {
+    showSuccess(email);
   }
 
-  if (password.value) {
-    if (isNaN(password.value) || password.value.length < 6) {
-      password.parentNode.classList.add("error");
-    } else {
-      password.parentNode.classList.add("success");
-    }
+  if (password.value === "") {
+    showError(password, "password is required");
+  } else {
+    showSuccess(password);
   }
 
-  if (password2.value) {
-    if (password.value !== password2.value) {
-      password.parentNode.classList.toggle("error");
-      password2.parentNode.classList.add("error");
-    } else {
-      password2.parentNode.classList.add("success");
-    }
+  if (password2.value === "") {
+    showError(password2, "password2 is required");
+  } else {
+    showSuccess(password2);
   }
 });
